8990552e85a465790547d3270fadaa77cd1c4fa3
vperin <perin.vinicius@outlook.com>
2022-07-12 09:33:26 -0500

    adding .ini again, and up folder

--- exercise3/data-via-api.py
+++ exercise3/data-via-api.py
@@ -1,1 +1,19 @@
-import os
+
+# https://hydrofunctions.readthedocs.io/en/latest/installation.html
+# National Water Dashboard https://dashboard.waterdata.usgs.gov/app/nwd/?region=lower48&aoi=default
+
+import pandas as pd
+import hydrofunctions as hf
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# flow 
+daily_mean = hf.NWIS(site = '0208734210',service = 'dv',start_date='2016-01-01',end_date='2022-01-01')
+daily_mean_values = daily_mean.df('q').shift(4, freq='H')
+
+fig, ax = plt.subplots(figsize=(20, 10))
+ax.step(daily_mean_values.loc['2016-01-01':'2022-01-01'].index.values, daily_mean_values.loc['2016-01-01':'2022-01-01'].values, where='post')
+plt.yscale('log')
+plt.show()
+
+# peak flow
